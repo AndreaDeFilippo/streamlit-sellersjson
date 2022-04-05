9d2ebb90a2dd7a09330625be91a929a213cae0b5
AndreaDeFilippo <59112023+AndreaDeFilippo@users.noreply.github.com>
2022-04-05 10:29:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,13 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 
-"""
-# Welcome to SellersJson to CSV tool!
-
-If you have any questions send an e-mail to [me](andrea.defilippo@showheroes-group.com).
-
-"""
-
-
 # Header
 
 response = requests.get('https://raw.githubusercontent.com/AndreaDeFilippo/icons/main/company/Logo-header_2.png')
 image = Image.open(BytesIO(response.content))
 st.image(image)
 
-st.title('SellersJson to CSV')
+st.title('SellersJson to CSV Tool')
 
 
 title = st.text_input('Company Domain', 'showheroes.com')
