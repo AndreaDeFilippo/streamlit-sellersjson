3fc988119d43d69450c60854ee2ef7a761a85fee
AndreaDeFilippo <59112023+AndreaDeFilippo@users.noreply.github.com>
2022-04-05 10:40:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,34 @@ st.title('SellersJson to CSV Tool')
 
 title = st.text_input('Company Domain', 'showheroes.com')
 
-df_sellers = pd.DataFrame()
 url = "https://us-central1-viralize-gateway.cloudfunctions.net/mergeSellers?providers=" + title
-df_json = pd.read_json(url, lines=True)
-df_sellers = df_sellers.append(df_json, ignore_index = True, sort=False)
+df = pd.read_json(url, lines=True)
 
-st.dataframe(df_sellers)
+
+rename = {'seller_id' : 'Seller ID',
+          'name' : 'Name',
+          'domain' : 'Domain',
+          'seller_type' : 'Seller Type',
+          'provider' : 'Player'}
+
+df = df.rename(columns=rename)
+
+columns = ['Player', 'Seller ID', 'Domain', 'Seller Type', 'Name']
+
+df = df[columns]
+
+st.dataframe(df)
+
+@st.cache
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
+
+csv = convert_df(df)
+
+st.download_button(
+     label="Download as CSV",
+     data=csv,
+     file_name= title + '-sellers.csv',
+     mime='text/csv',
+ )
